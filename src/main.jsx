fc0ec75310598dcf79167c656e87eb68cff0a1eb
jadhwik <adhwikjagadeesh@gmail.com>
2024-05-12 13:48:13 +0530

    files chnaged

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { HashRouter } from "react-router-dom";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <HashRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </HashRouter>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
+reportWebVitals();
